b6393768247813913a8f77772d05b70f4bb9dc84
nathanael <n.bellpactat@gmail.com>
2021-08-14 21:43:29 -0400

    Refactor test_flaskr.py for proper db cxn and tearDown db cleanup

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -4,7 +4,7 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
-from models import setup_db, Question, Category
+from models import cleanup_db, setup_db, Question, Category
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = f"postgresql://postgres:postgres@localhost:5432/{self.database_name}"
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -26,8 +26,9 @@ class TriviaTestCase(unittest.TestCase):
             self.db.create_all()
     
     def tearDown(self):
-        """Executed after reach test"""
-        pass
+        """Executed after each test"""
+        # Drop the database after each test to use the specific test data passed in during setup
+        cleanup_db(self.app, self.database_path)
 
     # TODO: Write at least one test for each test for successful operation and for expected errors.
 
